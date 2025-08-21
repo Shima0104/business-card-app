4a74b8ccf6498eb517c6b746e06eff99223d094e
Shima0104 <55shimablog@gmail.com>
2025-08-21 10:49:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UploadPage from './components/UploadPage';
 import CardPage from './components/CardPage';
+import CardEditor from './pages/CardEditor'; // 新しいパスから、インポート
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UploadPage />} />
-        {/* '/card' というパスを明確に定義する */}
-        <Route path="/card/:cardId" element={<CardPage />} />
+          <Route path="/" element={<CardEditor />} /> {/* ★ 新規作成ページ */}
+  <Route path="/edit/:cardId" element={<CardEditor />} /> {/* ★ 編集ページ */}
+  <Route path="/card/:cardId" element={<CardPage />} />
+
       </Routes>
     </Router>
   );
