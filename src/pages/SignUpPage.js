4fe8aa22f5769b73eb457cc1ea2591015c0106d9
Shima0104 <55shimablog@gmail.com>
2025-08-28 11:24:38 +0900

    Update SignUpPage.js

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase'; 
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { Box, Button, Paper, TextField, Typography } from '@mui/material';
 
 const SignUpPage = () => {
