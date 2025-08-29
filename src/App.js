8f188b056dae31f67a125fb5673519f3d790c91d
Shima0104 <55shimablog@gmail.com>
2025-08-29 11:39:14 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// ★ 我らが、創造した、すべての、世界（ページ）と、案内人を、召喚する
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute'; // ★
 import CardEditor from './pages/CardEditor';
 import CardPage from './components/CardPage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
-import ProtectedRoute from './components/ProtectedRoute'; // ★ 絶対障壁を、召喚
+import { useAuth } from './hooks/useAuth'; // ★
 
 function App() {
+  const { user, loading } = useAuth(); // ★ Navbarに渡すために、ここで魂の状態を確認
+
   return (
     <Router>
-      <Navbar />
+      <Navbar user={user} /> {/* ★ Navbarに、魂の、状態を、渡す */}
+      
       <Routes>
-        {/* --- 公開された、領域 --- */}
+        {/* --- Public Routes --- */}
         <Route path="/card/:cardId" element={<CardPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
 
-        {/* --- 保護された、聖域 --- */}
+        {/* --- Protected Routes --- */}
         <Route 
           path="/" 
           element={
@@ -44,25 +47,6 @@ function App() {
             </ProtectedRoute>
           } 
         />
-      </Routes>
-    </Router>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      {/* ★ 案内人は、常に、世界の、一番上に、存在する */}
-      <Navbar />
-      
-      {/* ★ 世界の、法則（ルート）は、案内人の、下に、存在する */}
-      <Routes>
-        <Route path="/" element={<CardEditor />} />
-        <Route path="/edit" element={<CardEditor />} /> {/* ★ /edit も、新規作成として、扱う */}
-        <Route path="/edit/:cardId" element={<CardEditor />} />
-        <Route path="/card/:cardId" element={<CardPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
