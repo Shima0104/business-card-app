42194d78bbfc1e9b218f75fd9375d0fb6d5eef79
Shima0104 <55shimablog@gmail.com>
2025-08-29 11:47:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useAuth } from './hooks/useAuth';
 
 import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute'; // ★
+import ProtectedRoute from './components/ProtectedRoute';
 import CardEditor from './pages/CardEditor';
 import CardPage from './components/CardPage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
-import { useAuth } from './hooks/useAuth'; // ★
 
 function App() {
-  const { user, loading } = useAuth(); // ★ Navbarに渡すために、ここで魂の状態を確認
+  const { user, loading } = useAuth();
 
   return (
     <Router>
-      <Navbar user={user} /> {/* ★ Navbarに、魂の、状態を、渡す */}
+      <Navbar user={user} />
       
       <Routes>
         {/* --- Public Routes --- */}
@@ -25,14 +25,6 @@ function App() {
         {/* --- Protected Routes --- */}
         <Route 
           path="/" 
-          element={
-            <ProtectedRoute>
-              <CardEditor />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/edit" 
           element={
             <ProtectedRoute>
               <CardEditor />
